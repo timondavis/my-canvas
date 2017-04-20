f192818d53644fc830f6aac827e2bed0b09090fa
Timon Davis <timondavis@yahoo.com>
2017-04-20 12:54:03 -0700

    [+] Fixing the rectangle one

--- src/app/canvas-window/canvas-window.component.ts
+++ src/app/canvas-window/canvas-window.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, Input } from '@angular/core';
 import { GameContext } from "../game-engine/game-context";
 import { GameInputObserver } from "../game-engine/game-input-observer";
 import { IsPointInPathGame } from "../Exercises/chapter-two-exercises/is-point-in-path/is-point-in-path-game";
+import { BasicRectanglesGame } from "../Exercises/chapter-two-exercises/basic-rectangles/basic-rectangles-game";
+import { PatternFillsGame } from "../Exercises/chapter-two-exercises/pattern-fills/pattern-fills-game";
+import { ColorsAndGradientsGame } from "../Exercises/chapter-two-exercises/colors-and-gradients/colors-and-gradients-game";
+import { SimpleTransformationsGame } from "../Exercises/chapter-two-exercises/simple-transformations/simple-transformations-game";
 
 @Component({
   selector: 'app-canvas-window',
@@ -15,7 +19,7 @@ export class CanvasWindowComponent implements OnInit  {
   public static buildGameContext() : GameContext {
 
     let context = new GameContext( new GameInputObserver() );
-    let game = new IsPointInPathGame( context );
+    let game = new SimpleTransformationsGame( context );
 
     context.setGame( game );
 
